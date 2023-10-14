3ab0a5ae555c115ea09f207eaf53c07265a9820d
jungwoo3490 <jungwoo3490@naver.com>
2023-10-15 01:04:11 +0900

    원형 큐를 활용한 은행 대기열 시뮬레이션 프로그램

--- Queue/bank_simulation.c
+++ Queue/bank_simulation.c
@@ -19,33 +19,28 @@ typedef struct { // 큐 타입
 } QueueType;
 
 // 오류 함수
-void error(char* message)
-{
+void error(char* message) {
 	fprintf(stderr, "%s\n", message);
 	exit(1);
 }
 
 // 공백 상태 검출 함수
-void init_queue(QueueType* q)
-{
+void init_queue(QueueType* q) {
 	q->front = q->rear = 0;
 }
 
 // 공백 상태 검출 함수
-int is_empty(QueueType* q)
-{
+int is_empty(QueueType* q) {
 	return (q->front == q->rear);
 }
 
 // 포화 상태 검출 함수
-int is_full(QueueType* q)
-{
+int is_full(QueueType* q) {
 	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
 }
 
 // 원형큐 출력 함수
-void queue_print(QueueType* q)
-{
+void queue_print(QueueType* q) {
 	printf("QUEUE(front=%d rear=%d) = ", q->front, q->rear);
 	if (!is_empty(q)) {
 		int i = q->front;
@@ -60,8 +55,7 @@ void queue_print(QueueType* q)
 }
 
 // 삽입 함수
-void enqueue(QueueType* q, element item)
-{
+void enqueue(QueueType* q, element item) {
 	if (is_full(q))
 		error("큐가 포화상태입니다");
 	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
@@ -69,8 +63,7 @@ void enqueue(QueueType* q, element item)
 }
 
 // 삭제 함수
-element dequeue(QueueType* q)
-{
+element dequeue(QueueType* q) {
 	if (is_empty(q))
 		error("큐가 공백상태입니다");
 	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
@@ -78,8 +71,7 @@ element dequeue(QueueType* q)
 }
 
 // 삭제 함수
-element peek(QueueType* q)
-{
+element peek(QueueType* q) {
 	if (is_empty(q))
 		error("큐가 공백상태입니다");
 	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
